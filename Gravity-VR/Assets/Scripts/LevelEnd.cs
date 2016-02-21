d546303940af8dd447f66e0d52beb9c12ae55df8
Tasnim Rahman <t.rahman94@gmail.com>
2016-02-21 12:30:54 -0600

    Proto level ending

---
+++ Gravity-VR/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEnd : MonoBehaviour {
+    private LevelInfo info;
+	// Use this for initialization
+	void Start () {
+        info = GameObject.FindGameObjectWithTag("Info").GetComponent<LevelInfo>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            info.LoadNext();
+        }
+    }
+}
---
+++ Gravity-VR/Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelInfo : MonoBehaviour {
+
+    public int level;
+    public int nextLevel;
+	// Use this for initialization
+	public void LoadNext()
+    {
+        Debug.Log("Loading next");
+        SceneManager.LoadScene(nextLevel);
+    }
+	
+    private void Restart()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
