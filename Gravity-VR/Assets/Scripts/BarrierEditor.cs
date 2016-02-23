21d1141f1d035891e0d46c2018fcabb22f2837f7
darargon <ackcrespo@sbcglobal.net>
2016-02-23 17:00:30 -0600

    Normals accurate on generated barriers

--- Gravity-VR/Assets/Scripts/BarrierEditor.cs
+++ Gravity-VR/Assets/Scripts/BarrierEditor.cs
@@ -23,26 +23,18 @@ public class BarrierEditor : MonoBehaviour
         SphericalCoordinates p3 = new SphericalCoordinates(radius, 0, 0, 1, radius + .3f, -Mathf.PI, Mathf.PI, -Mathf.PI / 2, Mathf.PI / 2);
         SphericalCoordinates p4 = new SphericalCoordinates(radius, 0, 0, 1, radius + .3f, -Mathf.PI, Mathf.PI, -Mathf.PI / 2, Mathf.PI / 2);
         int triBase;
-        int numVertices = (segments - 1) * 2 + 12;
+        int numVertices = (segments) * 4 + 28;
         int numTriangles = (4 * segments) + 16;
         float polarDif = (polarSize * Mathf.Deg2Rad) / 2;
         float eleDif = (elevationSize * Mathf.Deg2Rad) / 2f;
-        //Debug.Log(polarDif);
+
         p1.SetRotation(-polarDif, eleDif);
         p2.SetRotation(-polarDif, -eleDif);
         p3.SetRotation(polarDif, eleDif);
         p4.SetRotation(polarDif, -eleDif);
-        //.Log(p1.ToString());
-        //Debug.Log(p2.ToString());
-        //Debug.Log(p3.ToString());
-        //Debug.Log(p4.ToString());
         float polarStep = (polarSize * Mathf.Deg2Rad) / segments;
-        //polarStep = polarStep > 0 ? polarStep - 2 * Mathf.PI : polarStep;
 
         float eleStep = 0;//(p1.elevation - p4.elevation) / segments;
-        //set the smaller value to 0
-        //polarStep = Mathf.Abs(polarStep) > Mathf.Abs(eleStep) ? 0 : polarStep;
-        //eleStep = polarStep == 0 ? eleStep : 0;
 
         Vector3 p1Cart, p2Cart, p3Cart, p4Cart;
 
@@ -57,6 +49,7 @@ public class BarrierEditor : MonoBehaviour
         Vector3 leftMid = (p1Cart + p3Cart) / 2;
         Vector3 rightMid = (p2Cart + p4Cart) / 2;
         Vector3 center = (leftMid + rightMid) / 2;
+        //int v = 0;
         vertices[0] = p1Cart - center;
         vertices[1] = p2Cart - center;
         vertices[2] = p3Cart - center;
@@ -65,17 +58,22 @@ public class BarrierEditor : MonoBehaviour
         vertices[4] = leftMid - center;
         vertices[5] = rightMid - center;
 
-        //Vector3 norm = -center.normalized;// Vector3.Cross ((p1Cart - p2Cart), (p4Cart - p2Cart)).normalized;
+        Vector3 verticalNorm = -center.normalized;
         for (int i = 0; i < 6; i++)
         {
             //vertices[i + 6] = vertices[i] + height * norm;
-            vertices[i + 6] = vertices[i] - height * (vertices[i] + center).normalized;
+            vertices[i + 6] = vertices[i+12] = vertices[i] - height * (vertices[i] + center).normalized;
+            //normals[i + 6] = verticalNorm;
         }
         topLeft = vertices[6];
         topRight = vertices[7];
         bottomLeft = vertices[8];
         bottomRight = vertices[9];
-        //top rectangle
+        Vector3 farTopLeft = vertices[0];
+        Vector3 farTopRight = vertices[1];
+        Vector3 farBottomLeft = vertices[2];
+        Vector3 farBottomRight = vertices[3];
+        //front rectangle
         triangles[0] = 10;
         triangles[1] = 6;
         triangles[2] = 7;
@@ -93,21 +91,21 @@ public class BarrierEditor : MonoBehaviour
         triangles[11] = 9;
 
         //left side rectangle
-        triangles[12] = 10;
+        triangles[12] = 16;
         triangles[13] = 4;
-        triangles[14] = 6;
+        triangles[14] = 12;
 
-        triangles[15] = 6;
+        triangles[15] = 12;
         triangles[16] = 4;
         triangles[17] = 0;
 
         triangles[18] = 2;
         triangles[19] = 4;
-        triangles[20] = 10;
+        triangles[20] = 16;
 
-        triangles[21] = 8;
+        triangles[21] = 14;
         triangles[22] = 2;
-        triangles[23] = 10;
+        triangles[23] = 16;
 
         //right side rectangle
         for (int i = 0; i < 12; i += 3)
@@ -117,77 +115,97 @@ public class BarrierEditor : MonoBehaviour
             triangles[i + 26] = triangles[i + 13] + 1;
         }
 
-        //top rectangle
-        triangles[36] = 7;
-        triangles[37] = 6;
-        triangles[38] = 1;
+        vertices[18] = farTopLeft;
+        vertices[19] = farTopRight;
+        vertices[20] = farBottomLeft;
+        vertices[21] = farBottomRight;
+        vertices[22] = topLeft;
+        vertices[23] = topRight;
+        vertices[24] = bottomLeft;
+        vertices[25] = bottomRight;
 
-        triangles[39] = 1;
-        triangles[40] = 6;
-        triangles[41] = 0;
+        //top rectangle
+        triangles[36] = 23;
+        triangles[37] = 22;
+        triangles[38] = 19;
+
+        triangles[39] = 19;
+        triangles[40] = 22;
+        triangles[41] = 18;
 
         //bottom rectangle
-        triangles[42] = 8;
-        triangles[43] = 9;
-        triangles[44] = 2;
+        triangles[42] = 24;
+        triangles[43] = 25;
+        triangles[44] = 20;
 
-        triangles[45] = 2;
-        triangles[46] = 9;
-        triangles[47] = 3;
+        triangles[45] = 20;
+        triangles[46] = 25;
+        triangles[47] = 21;
 
-        int leftIdx = 0;
-        int rightIdx = 1;
+        vertices[26] = farTopLeft;
+        vertices[27] = farTopRight;
+        int roundleftIdx = 26;
+        int roundrightIdx = 27;
+        int flatLeftIdx = 0;
+        int flatRightIdx = 1;
         for (int i = 1; i < segments; i++)
         {
             SphericalCoordinates newLeft = p1.Rotate(polarStep, eleStep);
             SphericalCoordinates newRight = p2.Rotate(polarStep, eleStep);
-            Debug.Log(newLeft.ToString());
-            Debug.Log(newRight.ToString());
+            //Debug.Log(newLeft.ToString());
+            //Debug.Log(newRight.ToString());
             Vector3 nRCart = newRight.toCartesian - center;
             Vector3 nLCart = newLeft.toCartesian - center;
 
-            int nLIdx = 2 * i + 10;
-            int nRIdx = 2 * i + 11;
-            vertices[nLIdx] = nLCart;
-            vertices[nRIdx] = nRCart;
+            int nLIdx = 4 * i + 26;
+            int nRIdx = nLIdx+1;
+            int bLIdx = nRIdx + 1;
+            int bRIdx = bLIdx + 1;
+            vertices[nLIdx] = vertices[bLIdx] = nLCart;
+            vertices[nRIdx] = vertices [bRIdx] = nRCart;
 
             triBase = 12 * i + 36;
 
             triangles[triBase] = 4;
-            triangles[triBase + 1] = nLIdx;
-            triangles[triBase + 2] = leftIdx;
+            triangles[triBase + 1] = bLIdx;
+            triangles[triBase + 2] = flatLeftIdx;
 
-            triangles[triBase + 3] = leftIdx;
+            triangles[triBase + 3] = roundleftIdx;
             triangles[triBase + 4] = nLIdx;
             triangles[triBase + 5] = nRIdx;
 
-            triangles[triBase + 6] = leftIdx;
+            triangles[triBase + 6] = roundleftIdx;
             triangles[triBase + 7] = nRIdx;
-            triangles[triBase + 8] = rightIdx;
+            triangles[triBase + 8] = roundrightIdx;
 
             triangles[triBase + 9] = 5;
-            triangles[triBase + 10] = rightIdx;
-            triangles[triBase + 11] = nRIdx;
+            triangles[triBase + 10] = flatRightIdx;
+            triangles[triBase + 11] = bRIdx;
 
-            leftIdx = nLIdx;
-            rightIdx = nRIdx;
+            roundleftIdx = nLIdx;
+            roundrightIdx = nRIdx;
+            flatLeftIdx = bLIdx;
+            flatRightIdx = bRIdx;
         }
 
+        vertices[flatRightIdx + 1] = farBottomLeft;
+        vertices[flatRightIdx + 2] = farBottomRight;
+        Debug.Log(flatRightIdx + 2);
         triBase = (3 * numTriangles) - 12;
         triangles[triBase] = 4;
         triangles[triBase + 1] = 2;
-        triangles[triBase + 2] = leftIdx;
+        triangles[triBase + 2] = flatLeftIdx;
 
-        triangles[triBase + 3] = 3;
-        triangles[triBase + 4] = leftIdx;
-        triangles[triBase + 5] = 2;
+        triangles[triBase + 3] = flatRightIdx + 2;
+        triangles[triBase + 4] = roundleftIdx;
+        triangles[triBase + 5] = flatRightIdx + 1;
 
-        triangles[triBase + 6] = leftIdx;
-        triangles[triBase + 7] = 3;
-        triangles[triBase + 8] = rightIdx;
+        triangles[triBase + 6] = roundleftIdx;
+        triangles[triBase + 7] = flatRightIdx + 2;
+        triangles[triBase + 8] = roundrightIdx;
 
         triangles[triBase + 9] = 5;
-        triangles[triBase + 10] = rightIdx;
+        triangles[triBase + 10] = flatRightIdx;
         triangles[triBase + 11] = 3;
 
         //state = 'i';
@@ -195,7 +213,7 @@ public class BarrierEditor : MonoBehaviour
         mesh.name = "Barrier";
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        //mesh.RecalculateNormals();
+        mesh.RecalculateNormals();
 
         //GameObject next = Instantiate(prefab, center, Quaternion.identity) as GameObject;
         p1.SetRotation(polarAngle * Mathf.Deg2Rad, elevationAngle * Mathf.Deg2Rad);
@@ -203,7 +221,7 @@ public class BarrierEditor : MonoBehaviour
         transform.position = p1.toCartesian;
         GetComponent<MeshFilter>().sharedMesh = mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
-        CreateGlow();
+        //CreateGlow();
     }
 
     public void CreateGlow()
@@ -213,26 +231,26 @@ public class BarrierEditor : MonoBehaviour
 
         GameObject top = Instantiate(edgePrefab);
         top.transform.parent = transform;
-        top.transform.position = topLeft;
-        top.transform.rotation = Quaternion.Euler(180, 0, 0);
+        top.transform.localPosition = topLeft;
+        top.transform.localRotation = Quaternion.identity;
         top.GetComponent<VolumetricLineBehavior>().EndPos = length;
 
         GameObject bottom = Instantiate(edgePrefab);
         bottom.transform.parent = transform;
-        bottom.transform.position = bottomLeft;
-        bottom.transform.rotation = Quaternion.Euler(180, 0, 0);
+        bottom.transform.localPosition = bottomLeft;
+        bottom.transform.localRotation = Quaternion.identity;
         bottom.GetComponent<VolumetricLineBehavior>().EndPos = length;
 
         GameObject left = Instantiate(edgePrefab);
         left.transform.parent = transform;
-        left.transform.position = topLeft;
-        left.transform.rotation = Quaternion.Euler(90, 0, 0);
+        left.transform.localPosition = topLeft;
+        left.transform.localRotation = Quaternion.identity;
         left.GetComponent<VolumetricLineBehavior>().EndPos = height;
 
         GameObject right = Instantiate(edgePrefab);
         right.transform.parent = transform;
-        right.transform.position = topRight;
-        right.transform.rotation = Quaternion.Euler(90, 0, 0);
+        right.transform.localPosition = topRight;
+        right.transform.localRotation = Quaternion.identity;
         right.GetComponent<VolumetricLineBehavior>().EndPos = height;
 
         return;
