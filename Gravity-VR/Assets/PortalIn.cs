522ca23de4ae000dcf5ce33533160a1da9357b6b
darargon <ackcrespo@sbcglobal.net>
2016-03-11 22:55:11 -0600

    Level4 with Portals

---
+++ Gravity-VR/Assets/PortalIn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalIn : MonoBehaviour {
+
+    public GameObject outPortal;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("PortalIn Enter");
+        if (other.CompareTag("Player"))
+            //other.gameObject.GetComponent<Rigidbody>().MovePosition(outPortal.transform.position);
+            other.transform.position = outPortal.transform.position;
+    }
+}
