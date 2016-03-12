ba1e228e4681c18c3b446d7d1b6b03e3fff557fd
Tasnim Rahman <t.rahman94@gmail.com>
2016-03-11 23:54:18 -0600

    mostly working antiplayer

---
+++ Gravity-VR/Assets/Scripts/LaserBehaivor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBehaivor : MonoBehaviour {
+    public int direction;
+    public int sign;
+    public float radius;
+
+    private SphericalCoordinates curPos;
+    private float angularSpeed = .3f;
+
+	// Use this for initialization
+	void Start () {
+        curPos = new SphericalCoordinates(radius, 0, 0, 1, radius + .3f, -Mathf.PI, Mathf.PI, -Mathf.PI / 2, Mathf.PI / 2);
+        curPos.loopPolar = true;
+        Destroy(gameObject, 6f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        curPos.FromCartesian(gameObject.transform.position);
+        float degrees = angularSpeed * Time.deltaTime * sign;
+        curPos.RotatePolarAngle(degrees);
+        transform.position = curPos.toCartesian;
+    }
+}
