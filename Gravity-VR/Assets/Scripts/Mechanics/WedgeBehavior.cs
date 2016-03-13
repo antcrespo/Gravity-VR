55f56aea6f1cdfa0df5ff5638b29aace885494f8
Tasnim Rahman <t.rahman94@gmail.com>
2016-03-12 22:13:32 -0600

    rotate wedges + wedge borders

--- Gravity-VR/Assets/Scripts/Mechanics/WedgeBehavior.cs
+++ Gravity-VR/Assets/Scripts/Mechanics/WedgeBehavior.cs
@@ -1,17 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VolumetricLines;
 
 public class WedgeBehavior : MonoBehaviour {
 
     public BoxCollider bottom;
     public int sign;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Color startColor;
+
+    public bool selected = false;
+    private bool seen = false;
+
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("X"))
+        {
+            //Debug.Log("X pressed");
+            ProcessRotation();
+        }
+
+    }
+
+
+    public void ProcessRotation()
+    {
+        if (seen)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(0f, 0f, 90f);
+        }
+    }
+
+
+    public void OnPointer()
+    {
+        seen = !seen;
+        Color color = seen ? Color.yellow : startColor;
+        setLineColors(color);
+    }
+
+    private void setLineColors(Color color)
+    {
+        VolumetricLineBehavior[] lines = GetComponentsInChildren<VolumetricLineBehavior>();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i].LineColor = color;
+        }
+    }
+
+
 }
