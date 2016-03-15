ac4f3819d625027cc2d52d468fb4ed1ca14a55bd
darargon <ackcrespo@sbcglobal.net>
2016-03-14 23:00:54 -0500

    Removed broken screenshots, and added audio to portal

--- Gravity-VR/Assets/Scripts/PortalIn.cs
+++ Gravity-VR/Assets/Scripts/PortalIn.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class PortalIn : MonoBehaviour {
 
     public GameObject outPortal;
+    AudioSource source;
 	// Use this for initialization
 	void Start () {
-	    
+        source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +19,10 @@ public class PortalIn : MonoBehaviour {
     {
         //Debug.Log("PortalIn Enter");
         if (other.CompareTag("Player"))
+        {
             //other.gameObject.GetComponent<Rigidbody>().MovePosition(outPortal.transform.position);
             other.transform.position = outPortal.transform.position;
+            source.Play();
+        }
     }
 }
